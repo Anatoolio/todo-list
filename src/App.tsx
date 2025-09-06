f53f513971d9d04cb465c79820daea290a021173
Анатолий <Turkin.A.Vl@sberbank.ru>
2025-09-06 09:30:43 +0300

    ui fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import TodoFilter from "./components/TodoFilter";
 function App() {
   return (
     <StoreProvider>
-      <div className="max-w-xl mx-auto mt-10 p-4 shadow-lg bg-white text-slate-900 dark:bg-slate-800 dark:text-slate-100 rounded-lg">
-        <h1 className="text-3xl font-bold text-center mb-4">todos</h1>
-        <TodoInput />
-        <TodoList />
-        <TodoFilter />
+      <div className="min-h-screen flex items-center justify-center p-4">
+        <div className="max-w-xl w-full p-4 shadow-lg bg-white text-slate-900 dark:bg-slate-800 dark:text-slate-100 rounded-lg">
+          <h1 className="text-3xl font-bold text-center mb-4">todos</h1>
+          <TodoInput />
+          <TodoList />
+          <TodoFilter />
+        </div>
       </div>
     </StoreProvider>
   );
